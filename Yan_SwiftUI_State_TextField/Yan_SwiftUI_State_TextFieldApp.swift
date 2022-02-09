6fdf27c0dd67aebacf14a1d35d667a16ce9edabd
Kir Proto Q <kir.proto@ya.ru>
2022-02-09 09:39:20 +0500

    Initial Commit

---
+++ Yan_SwiftUI_State_TextField/Yan_SwiftUI_State_TextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_State_TextFieldApp.swift
+//  Yan_SwiftUI_State_TextField
+//
+//  Created by KIR Q on 2022.02.09.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_State_TextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
